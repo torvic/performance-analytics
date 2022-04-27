a5c4f1f8eee579b7a9692faaa0f3044004b69e47
torvic <130111@unsaac.edu.pe>
2022-04-27 12:12:11 -0500

    Add measure when web fonts are active

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ To view demo, create a file called `index.html` and write some code. Next, start
 * Add `<script>` tag immediately following the CSS that stores the current time.
 * Create a file named `analytics.js`.
 * Load the `analytics.js` script from `index.html`.
+
+## Step 2: Measuring when web fonts are active
+
+* We will use [webfont.js](https://github.com/typekit/webfontloader).
+* Update the main document to use `webfont.js`.
+* Create a promise  that is resolved once the font is loaded inside the file `analytics.js`.
--- analytics.js
+++ analytics.js
@@ -1,14 +1,45 @@
 window.onload = function () {
   measureCssUnblockTime()
+  measureWebfontPerfAndFailures()
 }
 
 function measureCssUnblockTime() {
   console.log('CSS', 'unblock', measureDuration('css:unblock'))
 }
 
+function measureWebfontPerfAndFailures() {
+  new Promise(function (resolve, reject) {
+    // The classes `wf-active` or `wf-inactive` are added to the <html>
+    // element once the fonts are loaded (or error).
+    let loaded = /wf-(in)?active/.exec(document.documentElement.className)
+    let success = loaded && !loaded[1] // No "in" in the capture group.
+    // If the fonts are already done loading, resolve immediately.
+    // Otherwise resolve/reject in the active/inactive callbacks, respectively.
+    if (loaded) {
+      success ? resolve() : reject()
+    } else {
+      let originalAciveCallback = WebFontConfig.active
+      WebFontConfig.inactive = reject
+      WebFontConfig.active = function () {
+        originalAciveCallback()
+        resolve()
+      }
+      // In case the webfont.js script fails to load, always reject the
+      // promise after the timeout amount.
+      setTimeout(reject, WebFontConfig.timeout)
+    }
+  })
+    .then(function () {
+      console.log('Fonts', 'active', measureDuration('fonts:active'))
+    })
+    .catch(function () {
+      console.error('Error loading web fonts')
+    })
+}
+
 function measureDuration(mark, opt_reference) {
-  var reference = opt_reference || 'responseEnd'
-  var name = reference + ':' + mark
+  let reference = opt_reference || 'responseEnd'
+  let name = reference + ':' + mark
 
   // Clears any existing measurements with the same name.
   performance.clearMeasures(name)
@@ -18,7 +49,7 @@ function measureDuration(mark, opt_reference) {
   performance.measure(name, reference, mark)
 
   // Gets the value of the measurement just created.
-  var measure = performance.getEntriesByName(name)[0]
+  let measure = performance.getEntriesByName(name)[0]
 
   // Returns the measure duration.
   return measure.duration
